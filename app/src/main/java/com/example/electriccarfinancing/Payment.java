b725fc03b15b61e2d82ce21c3b759f8a69902ff3
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-04-04 15:49:16 -0700

    final touches

--- app/src/main/java/com/example/electriccarfinancing/Payment.java
+++ app/src/main/java/com/example/electriccarfinancing/Payment.java
@@ -1,12 +1,18 @@
 package com.example.electriccarfinancing;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class Payment extends AppCompatActivity {
 
@@ -15,10 +21,39 @@ public class Payment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_payment);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView monthlyPayment = (TextView)findViewById(R.id.txtMonthlyPayment);
+        ImageView image = (ImageView)findViewById(R.id.imgYears);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        int intYears = sharedPref.getInt("key1", 0);
+        int intLoan = sharedPref.getInt("key2", 0);
+        float decInterest = sharedPref.getFloat("key3", 0);
+
+        float decMonthlyPayment;
+
+        decMonthlyPayment = (intLoan * (1 + (decInterest * intYears))) / (12 * intYears);
+        DecimalFormat currency = new DecimalFormat("$###,###.##");
+        monthlyPayment.setText("Monthly Payment " + currency.format(decMonthlyPayment));
+
+        if (intYears == 3) {
+            image.setImageResource(R.drawable.three);
+        } else if (intYears == 4) {
+            image.setImageResource(R.drawable.four);
+        } else if (intYears == 5) {
+            image.setImageResource(R.drawable.five);
+        } else {
+            monthlyPayment.setText("Enter 3, 4, or 5 years");
+        }
+
+
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
