67f3626bd89307c970a33723b0827251df6af7f0
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-04-03 20:29:40 -0700

    logic started on main activity

--- app/src/main/java/com/example/electriccarfinancing/MainActivity.java
+++ app/src/main/java/com/example/electriccarfinancing/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.electriccarfinancing;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int intYears;
+    int intLoan;
+    float decInterest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        final EditText years = (EditText)findViewById(R.id.txtYears);
+        final EditText loan = (EditText)findViewById(R.id.txtLoan);
+        final EditText interest = (EditText)findViewById(R.id.txtInterest);
+        Button button = (Button)findViewById(R.id.btnPayment);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intYears = Integer.parseInt(years.getText().toString());
+                intLoan = Integer.parseInt(loan.getText().toString());
+                decInterest = Float.parseFloat(interest.getText().toString());
+                
+            }
         });
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
